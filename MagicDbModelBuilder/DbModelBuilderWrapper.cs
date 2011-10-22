464e3fb8140dacd22b00f27633170411be5e4312
Maxime Beaudoin <beaudoin.maxime@gmail.com>
2011-10-21 21:25:26 -0400

    base

---
+++ MagicDbModelBuilder/DbModelBuilderWrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MagicDbModelBuilder
+{
+    public class DbModelBuilderWrapper
+    {
+        private readonly dynamic _builder;
+
+        public dynamic DbModelBuilder { get { return _builder; } }
+
+        public DbModelBuilderWrapper(DbModelBuilder builder)
+        {
+            _builder = builder;
+        }
+        
+        public EntityTypeConfigurationWrapper Entity(Type entityType)
+        {
+            return _builder.GetType()
+                .GetMethod("Entity")
+                .MakeGenericMethod(entityType)
+                .Invoke(_builder, null);
+        }
+    }
+}
---
+++ MagicDbModelBuilder/EntityTypeConfigurationWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MagicDbModelBuilder
+{
+    public class EntityTypeConfigurationWrapper
+    {
+        private readonly dynamic _config;
+        private readonly Type _type;
+
+        public dynamic EntityTypeConfiguration { get { return _config; } }
+        public Type GenericArgument { get { return _type; } }
+
+        public EntityTypeConfigurationWrapper(dynamic entityTypeConfiguration)
+        {
+            _config = entityTypeConfiguration;
+            _type = _config.GetType().GetGenericArguments()[0];
+        }
+
+        public EntityTypeConfigurationWrapper HasKey(Type keyType, string propertyName)
+        {
+            var paramEx = Expression.Parameter(_type, "x");
+            var lambdaEx = Expression.Lambda(Expression.Property(paramEx, propertyName), paramEx);
+
+            _config.GetType()
+                .GetMethod("HasKey")
+                .MakeGenericMethod(keyType)
+                .Invoke(_config, new[] { lambdaEx });
+
+            return _config;
+        }
+    }
+}
