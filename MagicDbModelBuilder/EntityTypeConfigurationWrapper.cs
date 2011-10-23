e4d9e3e560d54cf61c8c372fd2116bd283bde624
Maxime Beaudoin <beaudoin.maxime@gmail.com>
2011-10-22 21:19:46 -0400

    implemented the Property method

--- MagicDbModelBuilder/EntityTypeConfigurationWrapper.cs
+++ MagicDbModelBuilder/EntityTypeConfigurationWrapper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace MagicDbModelBuilder
 {
@@ -31,6 +34,51 @@ namespace MagicDbModelBuilder
                 .Invoke(_config, new[] { lambdaEx });
 
             return this;
+        }
+
+        public PrimitivePropertyConfiguration Property(Type propertyType, string propertyName)
+        {
+            var typeIsNullable = propertyType.IsGenericType &&
+                                 propertyType.GetGenericTypeDefinition() == typeof (Nullable<>);
+
+            MethodInfo[] methods = _config.GetType().GetMethods();
+            var candidates = methods.Where(m => m.Name == "Property" && m.IsGenericMethod);
+
+            var property = candidates.First(m =>
+            {
+                var param = m.GetParameters()[0];
+                var type = param.ParameterType;             // Expression<>
+                var func = type.GetGenericArguments()[0];   // Func<,>
+                var T = func.GetGenericArguments()[1];      // T or T?
+                var isNullable = T.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof(Nullable<>);
+                return (typeIsNullable && isNullable) || (!typeIsNullable && !isNullable);
+            });
+            
+            var paramEx = Expression.Parameter(_type, "x");
+            var lambdaEx = Expression.Lambda(Expression.Property(paramEx, propertyName), paramEx);
+
+            var nonNullableType = typeIsNullable ? propertyType.GetGenericArguments()[0] : propertyType;
+
+            return property
+                .MakeGenericMethod(nonNullableType)
+                .Invoke(_config, new[] { lambdaEx });
+        }
+
+        public DateTimePropertyConfiguration DateTimeProperty(string propertyName, bool nullable = false)
+        {
+            var datetimeType = nullable ? typeof (DateTime?) : typeof(DateTime);
+
+            var exType = typeof(Expression<>)
+                .MakeGenericType(typeof(Func<,>)
+                .MakeGenericType(_type, datetimeType));
+
+            var paramEx = Expression.Parameter(_type, "x");
+            var lambdaEx = Expression.Lambda(Expression.Property(paramEx, propertyName), paramEx);
+
+
+            return _config.GetType()
+                .GetMethod("Property", new[] { exType })
+                .Invoke(_config, new[] { lambdaEx });
         }
     }
 }
