34615fdbb2527c4ad8fa38326c11f2ba9ed9ed24
Maxime Beaudoin <beaudoin.maxime@gmail.com>
2011-10-24 12:27:53 -0400

    refactored DbModelBuilderWrapper into extension methods

---
+++ MagicDbModelBuilder.Tests/DbModelBuilderExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using NUnit.Framework;
+
+namespace MagicDbModelBuilder.Tests
+{
+    [TestFixture]
+    public class DbModelBuilderExtensionsTests
+    {
+        private readonly DbModelBuilder _builder;
+
+        public DbModelBuilderExtensionsTests()
+        {
+            _builder = new DbModelBuilder();
+        }
+
+        [Test]
+        public void Entity()
+        {
+            var config = _builder.Entity(typeof(Unicorn));
+
+            Assert.NotNull(config);
+            Assert.That(config.GetType() == typeof(EntityTypeConfigurationWrapper));
+            Assert.That(config.EntityTypeConfiguration.GetType() == typeof(EntityTypeConfiguration<Unicorn>));
+            Assert.That(config.GenericArgument == typeof(Unicorn));
+        }
+    }
+}
---
+++ MagicDbModelBuilder/DbModelBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MagicDbModelBuilder
+{
+    public static class DbModelBuilderExtensions
+    {
+        public static EntityTypeConfigurationWrapper Entity(this DbModelBuilder builder, Type entityType)
+        {
+            var config = builder.GetType()
+                .GetMethod("Entity")
+                .MakeGenericMethod(entityType)
+                .Invoke(builder, null);
+            return new EntityTypeConfigurationWrapper(config);
+        }
+    }
+}
